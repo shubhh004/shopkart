745d3f31502c4e66885352f43a96ed26a47fb543
shubhh004 <chaurasiashubh195@gmail.com>
2025-04-10 09:55:32 +0530

    initial commit

---
+++ src/main/java/io/github/shubhh004/shopkart/ShopkartApplication.java
@@ -0,0 +1,13 @@
+package io.github.shubhh004.shopkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopkartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/shubhh004/shopkart/ShopkartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shubhh004.shopkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
